f6b9f069dd37a4a5655aa59ab691d27b143ececc
vivia <vivia@10.1.167.16>
2018-09-12 15:14:18 -0400

    S

--- CSEHW2/src/ProductLoad.java
+++ CSEHW2/src/ProductLoad.java
@@ -1,7 +1,46 @@
 
 public class ProductLoad {
+	private String name;
+	private double weight;
+	private double value;
+	private boolean dangerous;
 
-	public ProductLoad(){
-		
+	public ProductLoad(String name, double weight, double value, boolean dangerous){
+		this.name = name;
+		this.weight = weight;
+		this.value = value;
+		this.dangerous = dangerous;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public boolean isDangerous() {
+		return dangerous;
+	}
+
+	public void setDangerous(boolean dangerous) {
+		this.dangerous = dangerous;
 	}
 }
--- CSEHW2/src/TrainCar.java
+++ CSEHW2/src/TrainCar.java
@@ -1,11 +1,38 @@
 
 public class TrainCar {
+	private double trainLength;
+	private double trainWeight;
+	private ProductLoad reference = null;
 	
-	public TrainCar() {
-		
+	public TrainCar(double trainLength, double trainWeight, ProductLoad reference) {
+		this.trainLength = trainLength;
+		this.trainWeight = trainWeight;
+		this.reference = reference;
 	}
+	
 	public boolean isEmpty() {
 		
+	}
+	
+	public double getTrainLength() {
+		return trainLength;
+	}
+	public void setTrainLength(double trainLength) {
+		this.trainLength = trainLength;
+	}
+	
+	public double getTrainWeight() {
+		return trainWeight;
+	}
+	public void setTrainWeight(double trainWeight) {
+		this.trainWeight = trainWeight;
+	}
+	
+	public ProductLoad getReference() {
+		return reference;
+	}
+	public void setReference(ProductLoad reference) {
+		this.reference = reference;
 	}
 
 }
